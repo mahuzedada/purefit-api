a172a6cd02b1ce9ec8d86bdcc4f07e02823dec11
Chatis Santos <chatis@afrointelligence.com>
2023-08-09 11:59:38 -0500

    added email

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Post, Body, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { OpenAIService } from './openai.service';
 import { DietPlanDTO } from './dto';
+import { EmailService } from './email.service';
 
 @Controller('ask')
 export class AppController {
   constructor(
     private readonly openaiService: OpenAIService,
+    private readonly emailService: EmailService,
     private readonly appService: AppService,
   ) {}
 
@@ -17,5 +19,13 @@ export class AppController {
   @Post()
   async generateDietPlan(@Body() data: DietPlanDTO): Promise<string> {
     return this.openaiService.generateDietPlan(data);
+  }
+  @Post('/email')
+  async sendEmail(@Body() body: any): Promise<void> {
+    return this.emailService.sendEmail(
+      'chatis@afrointelligence.com',
+      'PureFIT Recipes',
+      body.email,
+    );
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenAIService } from './openai.service';
 import { ConfigModule } from '@nestjs/config';
+import { EmailService } from './email.service';
 import 'dotenv/config';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService, OpenAIService],
+  providers: [AppService, OpenAIService, EmailService],
 })
 export class AppModule {}
