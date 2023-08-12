42e8ee1b79ec76999ecfafc2bca04d4f1d3ba8cc
Chatis Santos <chatis@afrointelligence.com>
2023-08-12 15:24:33 -0500

    updates

--- ecosystem.prod.config.js
+++ ecosystem.prod.config.js
@@ -5,11 +5,11 @@ module.exports = {
   apps: [
     {
       name: appName,
-      script: './dist/main.js',
+      script: './dist/src/main.js',
       env: {
-        NODE_ENV: 'production'
-      }
-    }
+        NODE_ENV: 'production',
+      },
+    },
   ],
 
   deploy: {
@@ -18,7 +18,7 @@ module.exports = {
       host: 'ec2-3-138-125-138.us-east-2.compute.amazonaws.com',
       ref: 'origin/master',
       'post-deploy': `${options['post-deploy']} && pm2 reload ecosystem.prod.config.js`,
-      path: join(appsDirectoryPath, appName)
-    }
-  }
+      path: join(appsDirectoryPath, appName),
+    },
+  },
 };
