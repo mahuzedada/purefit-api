0c2a7f75b5e90b086b9feb30cf9f83879d019b27
Chatis Santos <chatis@afrointelligence.com>
2023-08-12 15:22:19 -0500

    updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
     port: '22',
     repo: 'git@github.com:mahuzedada/purefit-api.git',
     'pre-deploy-local': '',
-    'post-deploy': 'npm run post-deploy'
+    'post-deploy': 'npm run post-deploy',
   },
   appName: require('./package.json').name,
-  appsDirectoryPath: '/home/ubuntu'
+  appsDirectoryPath: '/home/ubuntu',
 };
