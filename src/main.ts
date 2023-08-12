f5f3688eaa05fddefa720933fe761c4d945e924e
Chatis Santos <chatis@afrointelligence.com>
2023-08-12 14:22:40 -0500

    updates

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({ origin: 'https://ai.purefit.club' });
   await app.listen(4089);
 }
 bootstrap();
