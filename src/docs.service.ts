93f544b354e54850d655e6d813f78a6c928f59cf
Chatis Santos <chatis@afrointelligence.com>
2023-11-08 08:06:39 -0500

    updated to gpt4-turbo

--- src/docs.service.ts
+++ src/docs.service.ts
@@ -19,7 +19,7 @@ export class DocsService {
 
     try {
       const completion = await openai.chat.completions.create({
-        model: 'gpt-4',
+        model: 'gpt-4-1106-preview',
         messages: [{ content: prompt, role: 'user' }],
         temperature: 0,
         max_tokens: 3000,
@@ -55,7 +55,7 @@ Please generate a cover letter that incorporates all the provided details. The t
 
     try {
       const completion = await openai.chat.completions.create({
-        model: 'gpt-4',
+        model: 'gpt-4-1106-preview',
         messages: [{ content: prompt, role: 'user' }],
         temperature: 0,
         max_tokens: 3000,
@@ -77,13 +77,15 @@ Based on the provided candidate and job data:
       ${data.jdText}
       \`\`\`
 
-Please perform a skill gap analysis in the following json format.
+Perform a skill gap analysis in the following json format.
 {
   "strengths": "Highlight the candidate's strengths that match the job description. This should be formatted as a array of strings where each element is a strength",
   "gaps: "Identify areas where the candidate might be lacking or could improve in relation to the job's requirements. This should be formatted as a array of strings where each element is a gap",
   "resources": "For each identified skill gap or area of improvement, suggest free online resources or courses that can help the candidate acquire or bolster those required skills. The goal is to provide the candidate with a comprehensive action plan to better align their skills with the job's demands. This should be formatted as a array of {skill: 'skill1', resources: [{shortDescription: '', link: 'resource1'}, {shortDescription: '', link: 'resource2'}]} where each element is a skill and corresponding resources",
   "matchScore: "a score out of 100 to describe how well the candidate's skills are aligned with the job description",
+  "summary": "a summary of the skill gap analysis"
 }
+return only the json and nothing else. i have a program that expect the json output so returning extra texts around the json will create unwanted behaviors in my program
   `;
   }
 
